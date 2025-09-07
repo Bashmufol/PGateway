e6d43a19ea73e5993f605af31b9a08e00cdebeb0
Muhammed Bashir <bashmufol@gmail.com>
2025-09-07 08:27:43 +0100

    first commit

---
+++ src/main/java/com/bash/pgateway/PGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.bash.pgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PGateWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bash/pgateway/PGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.pgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
